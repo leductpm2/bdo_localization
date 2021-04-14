5873c3a14542e8a9350793ea38719557ae64ec0f
Duc Le <leductpm2@gmail.com>
2021-04-14 15:40:37 +0700

    Update Download_LOC.ps1

--- MainScript/Download_LOC.ps1
+++ MainScript/Download_LOC.ps1
@@ -6,13 +6,17 @@ function Unzip
 	[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-$version=(new-object net.webclient).DownloadString('http://akamai-gamecdn.blackdesertonline.com/live001/game/config/config.language.version')
-
+$versionFull=(new-object net.webclient).DownloadString('http://dn.sea.playblackdesert.com/UploadData/ads_files')
+$versionFull = $versionFull -replace "`t", "-"
+$versionFull = $versionFull -replace "`r`n", "-"
+$versionFull = $versionFull.Split("-")
+$version = $versionFull[3]
 Write-Host "Current version of English Loc: " $version
 
-$url_file = "http://akamai-gamecdn.blackdesertonline.com/live001/game/language/BDOLanguage_$version.zip"
+$url_file = "http://dn.sea.playblackdesert.com/UploadData/ads/languagedata_en/$version/languagedata_en.loc"
 Write-Host "Url for download new English Loc: " $url_file
 
+$output_loc = "$PSScriptRoot\..\TEMP\languagedata_en.loc"
 $output_Zip = "$PSScriptRoot\..\TEMP\BDOLanguage_$version.zip"
 $output_Folder = "$PSScriptRoot\..\TEMP"
 Write-Host "Output Zip directory: " $output_Zip
@@ -21,10 +25,10 @@ Write-Host "Output Folder directory: " $output_Folder
 if (!(Test-Path $output_Zip -PathType Leaf) -and !(Test-Path $output_Folder -PathType Leaf))
 {
 	$start_time = Get-Date
-	Invoke-WebRequest -Uri $url_file -OutFile $output_zip
+	Invoke-WebRequest -Uri $url_file -OutFile $output_loc
 	Write-Output "Download Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 	
-	Unzip $output_Zip $output_Folder
+	# Unzip $output_Zip $output_Folder
 }
 else
 {
